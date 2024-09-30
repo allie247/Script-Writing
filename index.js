e29a8fc5d73466e4d8394de26c5db5b7a7ad2e43
Allie Siclari <alliesiclari@yahoo.com>
2024-09-30 10:23:40 -0400

    Commit index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+//Memory
+
+//Create set of 12 cards.
+//Split the cards into pairs.
+//Assign unique graphics to one side of the pairs of cards.
+//Assign the same graphic to the other side of all of the cards.
+//Shuffle the cards.
+//Deal the cards out face down, with the universal graphic face up, in a 4x3 layout.
+//Assign players as player 1 and player 2. Player 1 will go first.
+
+//Loops until all of the cards are taken.
+    //Player one flips over two cards.
+    //If the graphics match, player one takes the cards.
+    //If the graphics do not match, the cards are put back where they were, face down.
+    //Player two flips over two cards.
+    //If the graphics match, player two takes the cards.
+    //If the graphics do not match, player two puts the cards back where they were, face down.
+
+//Players count how many cards they each have.
+//Whichever player has more cards wins.
+//Outcome can be a win, loss, or draw for each player.
